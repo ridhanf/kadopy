9edcaf50799fa2811d4ff981638f9f3baff0786b
Ridhan Fadhilah <ridhanfadhilah@gmail.com>
2019-10-20 22:46:10 +0700

    DONE HEROKU

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,9 +82,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'datakado',
+        'USER': 'deployer',
+        'PASSWORD': '022404tfUGM',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
